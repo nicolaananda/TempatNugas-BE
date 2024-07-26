138bfcbe28c8523bebde87467c46a18a452c7ee5
arifaeva <arifa.eva.ixa@gmail.com>
2024-07-26 23:13:27 +0700

    adding and fixing event feature

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -2,78 +2,100 @@ import { Request, Response } from "express";
 import { Event } from "../models/eventModel";
 
 export const eventController = {
-    // Buat event baru
-    createEvent: async (req: Request, res: Response) => {
-        console.log("createEvent function called"); // Logging untuk debugging
-        const { title, description, dateTime, eventWorkplace, eventAddress, userId, workplaceId } = req.body;
+  // Buat event baru
+  createEvent: async (req: Request, res: Response) => {
+    try {
+      console.log("createEvent function called"); // Logging untuk debugging
+      const { title, description, dateTime, userId, workplaceId } = req.body;
+      console.log(title, description, dateTime, userId, workplaceId);
 
-        try {
-            const createEvent = new Event({
-                title,
-                description,
-                dateTime,
-                eventWorkplace,
-                eventAddress,
-                userId,
-                workplaceId,
-            });
+      const createEvent = new Event({
+        title,
+        description,
+        dateTime,
+        userId,
+        workplaceId,
+      });
 
-            const savedEvent = await createEvent.save();
+      const savedEvent = await createEvent.save();
+      console.log(savedEvent);
 
-            res.status(201).json({
-                message: "User has successfully posted a new event",
-                data: savedEvent
-            });
-        } catch (error) {
-            res.status(500).json({
-                message: "Failed to post a new event",
-                data: null,
-            });
-        }
-    },
-
-    //  semua event
-    allEvents: async (req: Request, res: Response) => {
-        console.log("allEvents function called");
-        try {
-            const { id: workplaceId } = req.params;
-            const events = await Event.find({ workplaceId }).populate("userId", "name avatarUrl");
-            res.status(200).json({
-                message: "Successfully retrieved all events",
-                data: events
-            });
-        } catch (error) {
-            res.status(500).json({
-                message: "Failed to retrieve events",
-                data: null,
-            });
-        }
-    },
-
-    // event tunggal berdasarkan ID
-    eventSingle: async (req: Request, res: Response) => {
-        console.log("eventSingle function called");
-        const { id } = req.params;
-
-        try {
-            const { id: workplaceId, userId } = req.params;
-            const event = await Event.findOne({ _id: id, workplaceId, userId }).populate("userId", "name avatarUrl");
-            if (event) {
-                res.status(200).json({
-                    message: "Successfully retrieved the event",
-                    data: event
-                });
-            } else {
-                res.status(404).json({
-                    message: "Event not found",
-                    data: null
-                });
-            }
-        } catch (error) {
-            res.status(500).json({
-                message: "Failed to retrieve the event",
-                data: null,
-            });
-        }
+      return res.status(201).json({
+        message: "User has successfully posted a new event",
+        data: savedEvent,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Failed to post a new event",
+        data: null,
+      });
     }
+  },
+
+  //  semua event
+  allEvents: async (req: Request, res: Response) => {
+    console.log("allEvents function called");
+    try {
+      const events = await Event.find()
+        .populate("userId", "name avatarUrl")
+        .populate("workplaceId", "name address");
+
+      res.status(200).json({
+        message: "Successfully retrieved all events",
+        data: events,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Failed to retrieve events",
+        data: null,
+      });
+    }
+  },
+
+  // delete single event
+  deleteEvent: async (req: Request, res: Response) => {
+    try {
+      const { id: eventId } = req.params;
+
+      const deletedEvent = await Event.findOneAndDelete({
+        _id: eventId,
+      });
+      if (!deletedEvent) {
+        return res
+          .status(404)
+          .json({ status: 404, message: "Event not found" });
+      }
+
+      return res
+        .status(200)
+        .json({ status: 200, message: "Event deleted successfully" });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ status: 500, message: "Event deleting review", error });
+    }
+  },
+
+  //   event tunggal berdasarkan ID
+  eventSingle: async (req: Request, res: Response) => {
+    console.log("eventSingle function called");
+
+    try {
+      const { id: eventId } = req.params;
+      const event = await Event.findById({ _id: eventId })
+        .populate("userId", "name avatarUrl")
+        .populate("workplaceId", "name address");
+      if (!event) {
+        return res.status(404).json({
+          message: "Event not found",
+        });
+      }
+      return res.json(event);
+    } catch (error) {
+      return res.status(500).json({
+        message: "Failed to retrieve the event",
+        data: null,
+      });
+    }
+  },
 };
